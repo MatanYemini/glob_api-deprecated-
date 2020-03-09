4cbcb9faa4b0b6c185593e6cdf39509dc44d2304
Yemini <yeminimatan@gmail.com>
2020-03-09 14:48:47 +0200

    enabling all functions for posts handling

--- app.js
+++ app.js
@@ -1,62 +1,74 @@
-const express =require('express');
-const feedRoutes = require('./routes/feed');
-const bodyParser = require('body-parser');
-const app = express();
-const mongoose = require('mongoose');
 const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const multer = require('multer');
-
-const fileStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'images');
-    },
-    filename: (req, file, cb) => {
-        cb(null, new Date().toISOString() + '-' + file.originalname);
-    }
-});
-
-const fileFilter = (req, file, cb) => {
-    if (
-        file.mimetype === 'image/png' ||
-        file.mimetype === 'jpg' ||
-        file.mimetype === 'jpeg'
-    ) {
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }
-}
 
 const MONGODB_URI = 'mongodb+srv://Yemini:316386366@clusternode-gbgoy.mongodb.net/messages';
 const ACCESS_ORIGIN = 'Access-Control-Allow-Origin';
 const ACCESS_METHODS = 'Access-Control-Allow-Methods';
 const ACCESS_HEADERS = 'Access-Control-Allow-Headers';
 
+
+const feedRoutes = require('./routes/feed');
+
+const app = express();
+
+const uuidv4 = require('uuid/v4')
+ 
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'images');
+    },
+    filename: function(req, file, cb) {
+        cb(null, uuidv4())
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
-app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
+app.use(
+  multer({ storage: storage, fileFilter: fileFilter }).single('image')
+);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
-    res.setHeader(ACCESS_ORIGIN, '*');
-    res.setHeader(ACCESS_METHODS, 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader(ACCESS_HEADERS, 'Content-Type, Authorization');
-    next();
+  res.setHeader(ACCESS_ORIGIN, '*');
+  res.setHeader(
+    ACCESS_METHODS,
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader(ACCESS_HEADERS, 'Content-Type, Authorization');
+  next();
 });
 
 app.use('/feed', feedRoutes);
 
 app.use((error, req, res, next) => {
-    console.log(error);
-    const status = error.statusCode || 500;
-    const message = error.message;
-    res.status(status).json({message: message});
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
 });
 
-mongoose.connect(MONGODB_URI)
-    .then(res => {
-        app.listen(8080);
-        console.log('connected!');
-    })
-    .catch(err => {
-        console.log(err);
-    });
+mongoose
+  .connect(
+    MONGODB_URI
+  )
+  .then(result => {
+    app.listen(8080);
+  })
+  .catch(err => console.log(err));
